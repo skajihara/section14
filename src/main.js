0461a9e0f7486c43bd3d35e8a0d8a61a76dce01f
skajihara <t15cs015@gmail.com>
2024-04-03 13:49:59 +0900

    section14-4

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
 
-const app = createApp(App)
+import './assets/main.css'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { useCounterStore } from './stores/counter'
 
+const app = createApp(App)
 app.use(createPinia())
+const counterStore = useCounterStore()
+console.log('main.js', counterStore.count)
 
 app.mount('#app')
